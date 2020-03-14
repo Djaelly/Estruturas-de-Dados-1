2c98a985ba624f293df1946569c9bc34ff8a146b
Djaelly Santiago <djaellysantiago@gmail.com>
2020-03-14 17:30:01 -0300

    Q07

---
+++ Lista 1/Questao07/Questao07.java
@@ -0,0 +1,27 @@
+package Questao07;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Questao07 {
+
+	private static final float PI = 3.14f;
+
+	public static void main(String str[]) {
+		Scanner ler = new Scanner(System.in);
+		ler.useLocale(Locale.US);
+
+		int raio;
+
+		System.out.print("Digite o valor do raio: ");
+		raio = ler.nextInt();
+
+		if (raio != 0) {
+			System.out.printf("Area = " + "%.1f", PI * raio * raio);
+		} else {
+			System.out.println("Erro, Raio não pode ser 0");
+		}
+
+		ler.close();
+	}
+}
