387861b76d217987340f9e0aa726e7cf2c5d7fe3
Djaelly Santiago <djaellysantiago@gmail.com>
2020-03-15 20:41:10 -0300

    Testando o envio direto do eclipse

--- Lista 1/Questao07/Questao07.java
+++ Lista 1/Questao07/Questao07.java
@@ -1,25 +1,19 @@
 package Questao07;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Questao07 {
 
-	private static final float PI = 3.14f;
-
 	public static void main(String str[]) {
 		Scanner ler = new Scanner(System.in);
-		ler.useLocale(Locale.US);
+		int numero;
+		System.out.print("Digite um numero: ");
+		numero = ler.nextInt();
 
-		int raio;
-
-		System.out.print("Digite o valor do raio: ");
-		raio = ler.nextInt();
-
-		if (raio != 0) {
-			System.out.printf("Area = " + "%.1f", PI * raio * raio);
+		if (numero % 2 == 0) {
+			System.out.println("Par");
 		} else {
-			System.out.println("Erro, Raio não pode ser 0");
+			System.out.println("Impar");
 		}
 
 		ler.close();
